cdbf1a7863d52ab76b6459ac9107084361e865e9
Borys Meshkov <100526181+BorysMeshkov@users.noreply.github.com>
2023-07-03 11:14:02 +0300

    fix validation

--- src/db/models/validation-schemas/book-id-params.js
+++ src/db/models/validation-schemas/book-id-params.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 // can be replaced with custom validation using Mongo id checks
 const bookIdParamsSchema = Joi.object({
-  bookId: Joi.string().min(3).max(15).required(),
+  bookId: Joi.string().min(3).max(100).required(),
 });
 
 module.exports = bookIdParamsSchema;
